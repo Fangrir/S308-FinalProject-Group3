f98e7934042e9dd0c71ffa61ee7676e439567587
FunkeyMunkey <nelsonadiwira@gmail.com>
2017-12-04 12:30:52 -0500

    Room Mgmt layout & submit script, added Room class.

--- Group03/Room Mgmt.xaml.cs
+++ Group03/Room Mgmt.xaml.cs
@@ -22,6 +22,41 @@ namespace Group03
         public Room_Mgmt()
         {
             InitializeComponent();
+            // load file from json and insert into data grid
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            // declare variables
+            int intQuantity;
+            double dblPrice;
+
+            // check that quantity input is numeric
+            if (!Int32.TryParse(txtQuantity.Text.Trim(), out intQuantity))
+            {
+                MessageBox.Show("Quantity must be a whole number.");
+                return;
+            }
+
+            // assign quantity input to variable
+            intQuantity = Convert.ToInt32(txtQuantity.Text.Trim());
+
+            // check that price input is numeric
+            if (!Double.TryParse(txtPrice.Text.Trim(), out dblPrice))
+            {
+                MessageBox.Show("Price must be a number.");
+                return;
+            }
+
+            // assign price input to variable
+            dblPrice = Convert.ToDouble(txtPrice.Text.Trim());
+
+            // check if price is negative
+            if (dblPrice < 0)
+            {
+                MessageBox.Show("Price cannot be a negative number.");
+                return;
+            }
         }
     }
 }
