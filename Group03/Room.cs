88072609732e50c116a5ddf08f8845f779d7dfb0
FunkeyMunkey <nelsonadiwira@gmail.com>
2017-12-06 12:14:01 -0500

    Finished input validation, room management.

--- Group03/Room.cs
+++ Group03/Room.cs
@@ -8,6 +8,15 @@ namespace Group03
 {
     class Room
     {
-        public string roomType;
+        public string Type;
+        public int Quantity;
+        public double Price;
+
+        public Room(string type, int quantity, double price)
+        {
+            Type = type;
+            Quantity = quantity;
+            Price = price;
+        }
     }
 }
